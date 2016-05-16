6f34928f94fc1494f21042b581602ab8a8fc81d2
Sebastin Kolman <saby83@gmail.com>
2016-05-15 18:22:52 -0700

    - Added Parkway box  office data from 2012 - Modified parse-parkway to parse list of xlsx files

--- extract-parkway.js
+++ extract-parkway.js
@@ -1,9 +1,25 @@
+/**
+ * Gets a list of xlsx files in the data directory and parses the content to a text file.
+ *
+ * Stores the files as parkway-(date).txt
+ * */
 var XLSX = require('xlsx');
 var fs = require('fs');
-var workbook = XLSX.readFile('Nightly\ Box\ Office\ Report\ 03.18.16.xlsx');
-console.log(workbook.SheetNames[0]);
-var sheet1 = workbook.Sheets['Sheet1'];
-console.log(XLSX.utils.sheet_to_csv(sheet1));
-fs.writeFile('./parkway-report.txt', XLSX.utils.sheet_to_csv(sheet1), function(err){
-  console.log(err);
+var path = './data/';
+var files =  fs.readdirSync(path);
+files = files.filter(function(name) {
+  return !!name.match('xlsx');
+} );
+files.forEach(function(name, index, array) {
+
+  console.log('processing ', name);
+  var workbook = XLSX.readFile(path + name);
+  //console.log(workbook.SheetNames[0]);
+  var sheet1 = workbook.Sheets['Sheet1'];
+  var data = XLSX.utils.sheet_to_csv(sheet1);
+  var date = name.match(/\d{1,2}[\.\:]\d{1,2}[\.\:]\d{2,4}/g)[0];
+  fs.writeFile(path + 'parkway-'+ date +'.txt', data, function(err){
+    if(!err) return;
+    console.log('Error processing', name);
+  });
 });
