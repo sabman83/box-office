5bd8e604ef687775b5e290f94a5ab125655671db
Sebastin Kolman <saby83@gmail.com>
2016-06-02 00:38:54 -0700

    Added all parkway events to database

--- parse_parkway_calendar.rb
+++ parse_parkway_calendar.rb
@@ -4,7 +4,9 @@ require 'googleauth/stores/file_token_store'
 require 'date'
 require 'pry'
 require 'fileutils'
+require 'sqlite3'
 
+db = SQLite3::Database.new "box-office"
 OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
 APPLICATION_NAME = 'Parkway'
 CLIENT_SECRETS_PATH = 'client_secret.json'
@@ -40,40 +42,49 @@ def authorize
 end
 
 # Initialize the API
-service = Google::Apis::CalendarV3::CalendarService.new
-service.client_options.application_name = APPLICATION_NAME
-service.authorization = authorize
+$service = Google::Apis::CalendarV3::CalendarService.new
+$service.client_options.application_name = APPLICATION_NAME
+$service.authorization = authorize
 
 # Fetch the next 10 events for the user
 theater_1_calendar_id = 'thenewparkway.com_25uhqvcd5conik66ephgqe7j28@group.calendar.google.com'
 theater_2_calendar_id = 'thenewparkway.com_ighfjtidsap0o4oaubh7nfnqq0@group.calendar.google.com'
-start_date  = Time.new(2012,12,22).to_datetime
-end_date  = Time.new(2016,03,31).to_datetime
 IMDB_REGEX = /(tt\d+)/
 PRICE_REGEX = /[\s\S]*Admission Price:.*\$(\d+)/
-response_1 = service.list_events(theater_1_calendar_id,
+
+def get_events cal_id, token=nil
+  start_date  = Time.new(2012,12,22).to_datetime
+  end_date  = Time.new(2016,03,31).to_datetime
+
+  response = $service.list_events(cal_id,
                                max_results: 2500,
                                single_events: true,
                                order_by: 'startTime',
                                time_max: (end_date).rfc3339,
-                               time_min: (start_date).rfc3339)
-
-response_2 = service.list_events(theater_2_calendar_id,
-                               max_results: 2500,
-                               single_events: true,
-                               order_by: 'startTime',
                                time_min: (start_date).rfc3339,
-                               time_max: (end_date).rfc3339)
+                               page_token: token)
+  response
+end
 
+def get_all_events cal_id
+  page_token = nil
+  events = []
+  while true do
+    response_1 = get_events cal_id, page_token
+    events += response_1.items
+    break if response_1.next_page_token == nil || response_1.next_page_token.size == 0
+    page_token = response_1.next_page_token
+  end
+  events
+end
 
-puts "Upcoming events:"
-puts "No upcoming events found in theater 1" if response_1.items.empty?
-puts "No upcoming events found in theater 2" if response_2.items.empty?
-
-puts "NEXT PAGE TOKEN", response_1.next_page_token
-response = response_1.items + response_2.items
+theater_1_events = get_all_events theater_1_calendar_id
+theater_2_events = get_all_events theater_2_calendar_id
+events = theater_1_events + theater_2_events
 previous_price = 6
-response.each do |event|
+puts "TOTAL EVENTS ",  events.size
+binding.pry
+events.each do |event|
     movies = {}
     start = event.start.date || event.start.date_time
     #puts "SUMMARY: #{event.summary} - DESCRIPTION: #{event.description} (#{start})"
@@ -90,13 +101,13 @@ response.each do |event|
       puts 'No Price found for', event.summary
       price = previous_price
     else
-      previous_price = price
+      previous_price = price[1]
       price = price[1]
     end
-    imdb_id = imdb_id[1]
+    imdb_id = imdb_id
     movies[imdb_id] ||= {}
     movies[imdb_id]['name'] = movie_name
-    movies[imdb_id]['price'] = price || previous_price
+    movies[imdb_id]['price'] = price
     if start.class.to_s == 'DateTime'
       movies[imdb_id]['time'] = start.strftime('%H:%M')
       movies[imdb_id]['date'] = start.to_date.to_s
@@ -105,5 +116,16 @@ response.each do |event|
       movies[imdb_id]['date'] = start.to_s
     end
     movies[imdb_id]['theater'] = event.organizer.email == theater_1_calendar_id ? 1 : 2
+    begin
+
+      db.execute('INSERT INTO parkway_daily_shows(imdb_id, name, theater, time,  date, price) VALUES (?,?,?,?,?,?)',
+                                                              imdb_id, movie_name, movies[imdb_id]['theater'],
+                                                              movies[imdb_id]['time'],
+                                                              movies[imdb_id]['date'],
+                                                               movies[imdb_id]['price'])
+    rescue
+      binding.pry
+    end
     #puts movies
 end
+db.close
