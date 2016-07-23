d48296e3b31b6f446ac6eb7467eb3efcf3788b0b
Sebastin Kolman <saby83@gmail.com>
2016-07-23 15:11:53 -0700

    Correct data for 11-20-2015, added sql and csv data

---
+++ data/joined_data.sql
@@ -0,0 +1,4 @@
+select shows.imdb_id, shows.name, gross.gross, gross.date,  shows.theater, shows.time, shows.date, count(shows.date) as num_of_shows, avg(shows.price) as ticket_price  from parkway_daily_performance as gross 
+				join parkway_movies as movies on gross.movie_id = movies.id
+				join parkway_daily_shows  as shows on movies.imdb_id = shows.imdb_id and gross.date = shows.date group by shows.imdb_id, shows.date order by shows.imdb_id,shows.date desc
+						
