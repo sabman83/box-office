559ef5c788744a6bc6e6f270f13e2916adc6735d
Sebastin Kolman <saby83@gmail.com>
2017-05-30 23:52:30 -0700

    manualy updated imdb ids

--- get_imdb_id_from_shows_to_movies.rb
+++ get_imdb_id_from_shows_to_movies.rb
@@ -5,47 +5,47 @@ require 'pry'
 
 $db = SQLite3::Database.new "box-office"
 
-def store_id_for_movie(rows, imdb_id)
-  rows.each do |movie|
-    $db.execute("UPDATE parkway_daily_shows SET imdb_id=? where name=?", imdb_id, movie[1])
-  end
-end
-
-def revert_imdb_id(id, imdb_id)
+def update_imdb_id_in_parkway_movies(id, imdb_id)
     $db.execute("UPDATE parkway_movies SET imdb_id=? where id=?", imdb_id, id)
 end
+
+
 #get list of movies with no imdb ids
-movies = $db.execute('SELECT id,name,imdb_id from parkway_movies where imdb_id not in (select distinct imdb_id from parkway_daily_shows where imdb_id like "tt%")')
-binding.pry
+movies = $db.execute('select * from parkway_movies where imdb_id is null or imdb_id = ""')
 
 #for each movie , get matching row,
 movies.each do |movie|
   begin
-    results = $db.execute("SELECT imdb_id,name from parkway_daily_shows where name like \"%"+ movie[1] +"%\"")
+    results = $db.execute("select imdb_id,name from parkway_daily_shows where name like '%#{movie[1]}%' group by imdb_id")
+
     if results.size == 0
       pp '**************** NO DATA FOUND FOR ' +  movie[1] + ' *****************'
       next
     end
     if !results.nil?
-      print 'MORE THAN ONE ENTRY FOUND FOR ', movie
-      pp 'Following matches found: *****************************************'
-      results.each_with_index do |result, index|
-        pp index.to_s + ' : ' + result[0] + ' ' + result[1]
-      end
-      pp 'Y to update all or N to ignore or R to revert'
-      idx = gets.chomp
-      idx = idx.to_s
-      next if idx == 'n' || idx == 'N'
-      if idx == 'Y' || idx == 'y'
-        store_id_for_movie(results, movie[2])
+      if results.size == 1
+        update_imdb_id_in_parkway_movies(movie[0],results[0][0])
       else
-        revert_imdb_id(movie[0],results[idx.to_i][0])
+        next # if more than one entry found correct it manually
+        print 'MORE THAN ONE ENTRY FOUND FOR ', movie
+        pp 'Following matches found: *****************************************'
+        results.each_with_index do |result, index|
+          pp index.to_s + ' : ' + result[0] + ' ' + result[1]
+        end
+        pp 'Enter Number or Enter Y to update all or N to ignore'
+        idx = gets.chomp
+        idx = idx.to_s
+        next if idx == 'n' || idx == 'N'
+        if idx == 'Y' || idx == 'y'
+          results.each do |result|
+            update_imdb_id_in_parkway_movies(movie[0],result[0])
+          end
+        end
       end
-      next
     end
   rescue => e
     pp e
-    binding.pry
+    next
   end
 end
 
